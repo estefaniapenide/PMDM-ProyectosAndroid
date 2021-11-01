72bcacf2eb28cd6a69dc49ca845eb2069967ac8c
Estefanía Penide Casanova <72271327+estefaniapenide@users.noreply.github.com>
2021-11-01 19:00:46 +0100

    Update Operacion.java

--- CalculadoraFuncionamiento/src/main/java/com/mycompany/calculadora/Operacion.java
+++ CalculadoraFuncionamiento/src/main/java/com/mycompany/calculadora/Operacion.java
@@ -1,7 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package com.mycompany.calculadora;
 
@@ -10,5 +6,48 @@ package com.mycompany.calculadora;
  * @author a20estefaniapc
  */
 class Operacion {
+    
+    public static double numero1;
+    public static double numero2;
+    public static double resultado;
+    public static String signo;
+    
+    public static double suma(double numero1, double numero2){
+    return numero1+numero2;
+    }
+    
+    public static double resta(double numero1, double numero2){
+    return numero1-numero2;
+    }
+    
+    public static double mult(double numero1, double numero2){
+    return numero1*numero2;
+    }
+    
+    public static double div(double numero1, double numero2){
+    return numero1/numero2;
+    }
+    
+       public static double elegiroperacion(double numero1, double numero2, String signo){
+        switch(signo){
+            case "+":
+                resultado=Operacion.suma(numero1, numero2);
+                break;
+            case "-":
+                resultado=Operacion.resta(numero1, numero2);
+                break;
+            case "*":
+                resultado=Operacion.mult(numero1, numero2);
+                break;
+            case "/":
+                resultado=Operacion.div(numero1, numero2);
+                break;
+            default:
+            break;
+        }
+                
+    return resultado;
+    }
+    
     
 }
